3db8f3d81e5868c4ac55d28691136ffc700e8a59
WayneSzchenTFS <szchen@tfs.ca>
2022-11-04 09:11:23 -0400

    Nov 4 2022

--- app/src/main/java/com/szchen/convertisseur/MainActivity.java
+++ app/src/main/java/com/szchen/convertisseur/MainActivity.java
@@ -2,9 +2,33 @@ package com.szchen.convertisseur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    public void convert(View view)
+    {
+        Double canadianAmount;
+        Double americanAmount;
+        editText=findViewById(R.id.editTextNumber);
+        canadianAmount=Double.parseDouble(editText.getText().toString());
+        americanAmount=canadianAmount*0.76;
+
+        // cacher le clavier
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        Toast.makeText(MainActivity.this, String.format("%.2f", americanAmount) + "$ US", Toast.LENGTH_LONG).show();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
